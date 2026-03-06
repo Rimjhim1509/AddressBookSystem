0c3b5e16da1286ccb86f249696861455cf8ea4cc
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-03-06 15:14:49 +0530

    [Rimjhim] : Added the list of contact in address Book

--- src/main/java/com/example/demo/AddressBookApplication.java
+++ src/main/java/com/example/demo/AddressBookApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.service.AddressBook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AddressBookApplication.class, args);
+		System.out.println("Welcome to Address Book System");
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContact();
 	}
 
 }
